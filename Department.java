0b0134c354400828741624fc65127a7082dc95b2
okplj <omar.a.a.t1427@gmail.com>
2026-03-31 11:57:48 +0300

    Update Department.java
    remove

--- Department.java
+++ Department.java
@@ -11,9 +11,6 @@ public class Department {
         profList = new Professors[numOFProfessors];
     }
 
-    public Department() {
-
-    }
 
     public void Search_professor (String d) {
         boolean f = true;
